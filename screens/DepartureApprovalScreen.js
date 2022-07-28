f4daa792f9628a37481528dcaeb01f97adaa622c
Tharindu7Kanchana <7tharindugalle@gmail.com>
2022-07-28 23:20:55 +0530

    update

--- screens/DepartureApprovalScreen.js
+++ screens/DepartureApprovalScreen.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 const DepartureApprovalScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Departure Approval Screen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Departure Approval Screen</Text>
+      <Button title="Click Here" onPress={() => alert('Button Clicked!')} />
+    </View>
+  );
 };
 
 export default DepartureApprovalScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- screens/FishermanRegistration.js
+++ screens/FishermanRegistration.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
 const FishermanRegistration = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Fisherman Registration</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Fisherman Registration</Text>
+      <Button title="Click Here" onPress={() => alert('Button Clicked!')} />
+    </View>
+  );
 };
 
 export default FishermanRegistration;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
